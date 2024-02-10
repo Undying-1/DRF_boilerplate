802bbd453407dd63d6c68c6f2c2df496faeffc92
Undying-1 <bear.lvb@gmail.com>
2024-02-10 20:28:55 +0100

    Modifed: jazzmin.py, schema.py

--- core/jazzmin.py
+++ core/jazzmin.py
@@ -2,7 +2,7 @@
 
 JAZZMIN_SETTINGS: dict = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "KingDesignn Studio",
+    "site_title": "",
     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_header": "KingDesignn Studio",
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
